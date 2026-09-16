488441d5e5f0162b02776e6166ba4783dba393f0
Arturo Erdély <arturoerdely@gmail.com>
2026-09-16 10:06:44 -0600

    Update vi.jl

--- código/vi.jl
+++ código/vi.jl
@@ -1,24 +1,24 @@
 # Autor: Arturo Erdely
-# Fecha: 15-SEP-2026
+# Fecha: 16-SEP-2026
 
 """
-    vi(archivo::String, num::Int = 1, ini::Int = 1, fin::Int = 0)
+    vi(archivo::AbstractString; num::Bool = true, ini::Int = 1, fin::Int = 0)
 
-Mostrar en la terminal el contenido de `archivo` numerando cada línea, comenzando por la línea `ini` (la primera, si no se especifica) y terminando en la línea `fin` (o la última, si no se especifica). Si `num = 1` (default) se agrega numeración.
+Mostrar en la terminal el contenido de `archivo` línea por línea, comenzando por la línea `ini` (la primera, si no se especifica) y terminando en la línea `fin` (o la última, si no se especifica). Si `num = true` (default) se agrega numeración a cada línea.
 """
-function vi(archivo::String, num::Int = 1, ini::Int = 1, fin::Int = 0)
-	c = collect(enumerate(readlines(archivo)))
-	if fin == 0
-		fin = length(c)
-	end
-	if num == 1
-		for i in ini:fin
-			println(string(c[i][1]), "  ", c[i][2])
-		end
-	else
-		for i in ini:fin
-			println(c[i][2])
-		end
-	end
-	return nothing
+function vi(archivo::AbstractString; num::Bool = true, ini::Int = 1, fin::Int = 0)
+
+    lineas = readlines(archivo)
+    n = length(lineas)
+
+    fin = fin == 0 ? n : min(fin, n)
+    ini = max(ini, 1)
+
+    ancho = ndigits(fin)
+
+    for i in ini:fin
+        num ? println(lpad(string(i), ancho), "  ", lineas[i]) : println(lineas[i])
+    end
+
+    return nothing
 end
